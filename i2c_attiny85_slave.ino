336c5bde045a06a143a6270b9daf9e5ef6b6d941
Juan Pleitez <juanpc13lolol@gmail.com>
2018-08-04 18:47:55 -0600

    Ejemplo sencillo de esclavo

--- i2c_attiny85_slave.ino
+++ i2c_attiny85_slave.ino
@@ -1,20 +1,49 @@
 #include "TinyWire.h"
 
 const byte own_address = 13;
+//Pines y variables
+#define pinBtn1 1
+#define pinBtn2 3
+#define pinLec A2
+int lectura = 0;
+byte selector = 0;
 
 void setup() {
   TinyWire.begin(own_address);
   TinyWire.onRequest(onI2CRequest);
   TinyWire.onReceive(onI2CReceive);
+  pinMode(pinBtn1, INPUT);
+  pinMode(pinBtn2, INPUT);
+  pinMode(pinLec, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //Almacenando
+  lectura = analogRead(pinLec);
 }
 
 void onI2CReceive(int howMany) {
+  while (TinyWire.available()) {
+    selector = TinyWire.receive();
+  }
 }
 
 void onI2CRequest() {
+  int data = 0;
+  switch (selector) {
+    case 0:
+      data = own_address;
+      break;
+    case 1:
+      data = digitalRead(pinBtn1);
+      break;
+    case 3:
+      data = digitalRead(pinBtn2);
+      break;
+    case 4:
+      data = lectura;
+      break;
+  }
+  TinyWire.write(data >> 2);
+  TinyWire.write(data & 255);
 }
