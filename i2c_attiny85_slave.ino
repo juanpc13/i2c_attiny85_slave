c69ed4f459f36f8e82ee3c0cdbc9f3b7768be205
Juan Pleitez <juanpc13lolol@gmail.com>
2018-08-04 18:43:05 -0600

    Agregando TinyWire clases de lucullusTheOnly

--- i2c_attiny85_slave.ino
+++ i2c_attiny85_slave.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "TinyWire.h"
 
+const byte own_address = 13;
+
+void setup() {
+  TinyWire.begin(own_address);
+  TinyWire.onRequest(onI2CRequest);
+  TinyWire.onReceive(onI2CReceive);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void onI2CReceive(int howMany) {
+}
+
+void onI2CRequest() {
 }
